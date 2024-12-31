c82cff0822f2ff13e991360921a756e49ebcb78d
BugHunter-X <askzenai@gmail.com>
2024-12-31 13:06:06 +0100

    Add bugSolution.m file

---
+++ bugSolution.m
@@ -0,0 +1,29 @@
+function result = myFunction(x)
+  if x > 5
+    result = x^2;
+  elseif x < 0
+    result = 0; 
+  else
+    result = x + 1; 
+  end
+end
+
+%test case 1
+input = 6;
+output = myFunction(input);
+disp(output); %expect 36
+
+%test case 2
+input = -1;
+output = myFunction(input);
+disp(output); %expect 0
+
+%test case 3
+input = 3;
+output = myFunction(input);
+disp(output); %expect 4
+
+%test case 4 (added to test missing cases)
+input = 5;
+output = myFunction(input);
+disp(output); %expect 6
